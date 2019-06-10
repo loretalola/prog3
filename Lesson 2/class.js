540eeacd7aba509ba0a9d36ec6ae4d0253d10d7e
loretalola <loreta.hakobyan.y@tumo.org>
2019-06-10 14:51:25 +0400

    l

--- Lesson 2/class.js
+++ Lesson 2/class.js
@@ -47,12 +47,10 @@ class Grass extends LivingCreature {
   
 
 
-class GrassEater {
+class GrassEater  extends LivingCreature {
     constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
-        this.index = index;
-        this.energy = 5;
+        super(x,y,index);
+        this.energy = 8;
     }
 
     getNewCoordinates() {
@@ -165,13 +163,13 @@ class GrassEater {
 
 }
 
-class Predator {
+class Predator extends LivingCreature {
     constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
-        this.index = index;
+        super(x,y,index);
         this.energy = 7;
     }
+       
+    
     getNewCoordinates() {
         this.directions = [
             [this.x - 1, this.y - 1],
@@ -185,21 +183,11 @@ class Predator {
         ];
     }
         
+    chooseCell(ch) {
+        this.getNewCoordinates();
+        return super.chooseCell(ch);
+     }
 
-chooseCell(character) {
-    this.getNewCoordinates();
-    var found = [];
-    for (var i in this.directions) {
-        var x = this.directions[i][0];
-        var y = this.directions[i][1];
-        if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-            if (matrix[y][x] == character) {
-                found.push(this.directions[i]);
-            }
-        }
-    }
-    return found;
-}
 move() {
 
     var newCell = random(this.chooseCell(0));
