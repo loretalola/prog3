35c6264da45166fb8b83946079c53309c4a9cc29
loretalola <loreta.hakobyan.y@tumo.org>
2019-06-10 21:03:59 +0400

    homework

---
+++ Final work/Predator.js
@@ -0,0 +1,100 @@
+module.exports = class Predator {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.energy = 6;
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    move() {
+
+        var newCell = random(this.chooseCell(0));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[this.y][this.x] = 0;
+            matrix[newY][newX] = 3;
+
+
+            this.y = newY;
+            this.x = newX;
+            this.energy--;
+
+        }
+
+    }
+    eat() {
+        var newCell = random(this.chooseCell(2));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[this.y][this.x] = 0;
+            matrix[newY][newX] = 3;
+
+            for (var i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1);
+                    break;
+                }
+            }
+
+
+            this.y = newY;
+            this.x = newX;
+            this.energy += 3;
+
+        }
+
+    }
+    mul() {
+
+        var newCell = random(this.chooseCell(0));
+
+        if (this.energy >= 8 && newCell) {
+            var newPredator = new Predator(newCell[0], newCell[1], this.index);
+            PredatorArr.push(newPredator);
+            matrix[newCell[1]][newCell[0]] = 3;
+            this.energy = 6;
+        }
+    }
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0;
+            for (var i in PredatorArr) {
+                if (this.X == PredatorArr[i].x && this.y == PredatorArr[i].y) {
+                    PredatorArr.splice(i, 1)
+
+                }
+            }
+        }
+    }
+}
